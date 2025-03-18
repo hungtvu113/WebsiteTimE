4332b3674c20de0fa62d8b3bd73b86b103185448
hungtvu113 <tranhunggit@gmail.com>
2025-03-19 00:54:19 +0700

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,7 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+// Vô hiệu hóa toàn bộ ESLint
+const eslintConfig = [];
 
 export default eslintConfig;
